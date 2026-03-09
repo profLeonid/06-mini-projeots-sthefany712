98fdaf5c0e3e65b215291587acbb4e26f959ec7f
STHEFANY DA SILVA CARMO CORREIA <25203734@CFP127ALUNO.LOCAL>
2026-03-09 11:25:19 -0300

    Iniciando atividade

---
+++ 02-calculadora-desconto/app.js
@@ -0,0 +1,15 @@
+'use strict'
+function calcular() {
+    const preco = document.getElementById('valor')
+    const desconto = document.getElementById('desconto')
+    const resultado = document.getElementById('resultado')
+
+    if(preco.value && desconto.value) {
+        
+
+    }
+    
+}
+
+
+
