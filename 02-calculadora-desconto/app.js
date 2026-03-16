68bfe21336ee965768c34284ffd3c02f7b2e6a31
STHEFANY DA SILVA CARMO CORREIA <25203734@CFP127ALUNO.LOCAL>
2026-03-16 10:24:03 -0300

    continuação da atividade

--- 02-calculadora-desconto/app.js
+++ 02-calculadora-desconto/app.js
@@ -1,15 +1,38 @@
 'use strict'
-function calcular() {
-    const preco = document.getElementById('valor')
-    const desconto = document.getElementById('desconto')
-    const resultado = document.getElementById('resultado')
 
-    if(preco.value && desconto.value) {
-        
+function trocarCor() {
+    const resultado = document.getElementById('resultado')
+    if(desconto < 5 ){
+        resultado.classList.add('desconto1')
+    }else if (desconto <= 10){
+        resultado.classList.add('desconto2')
+    }else{
+        resultado.classList.add('desconto3')
 
     }
-    
 }
 
+//função em seta 
+const calcularDesconto = (preco, desconto) => preco * desconto / 100
+
+
+// function calcularDesconto(preco, desconto) {
+//     const valorEconomizado = preco * desconto / 100
+//     console.log(valorEconomizado)
+//     return valorEconomizado
+// }
+
+function mostrarResultados (){
+    const preco = Number(document.getElementById('preco').value)
+    const desconto = Number(document.getElementById('desconto').value)
+    const resultado = document.getElementById('resultado')
+    calcularDesconto(preco, desconto)
+    resultado.textContent = calcularDesconto(preco,desconto)
+    trocarCor()
+
+}
+
+document.getElementById('calcular')
+        .addEventListener('click', mostrarResultados)
 
 
---
+++ 03-calculadora-media/app.js
@@ -0,0 +1,22 @@
+'use strict'
+function obterNotas() {
+    const number1 = document.getElementById('number1')
+    const number2 = document.getElementById('number2')
+    const number3 = document.getElementById('number3')
+
+    if(number1 <0 || number1 >10){
+        return
+    }
+
+    if(number2 <0 || number2 >10){
+        return
+    }
+
+    if(number3 <0 || number3 >10){
+        return
+    }
+
+
+
+    
+}
