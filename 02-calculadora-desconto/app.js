0b42521686f2f9d18d4da2f49ede39100ff84664
STHEFANY DA SILVA CARMO CORREIA <25203734@CFP127ALUNO.LOCAL>
2026-03-23 08:08:46 -0300

    continuação

--- 02-calculadora-desconto/app.js
+++ 02-calculadora-desconto/app.js
@@ -9,12 +9,14 @@ function trocarCor() {
     }else{
         resultado.classList.add('desconto3')
 
-    }
+    } 
 }
+//A classList no JavaScript é uma propriedade somente leitura que retorna uma coleção ativa (DOMTokenList) das classes CSS de um elemento HTML. 
+    // Ela facilita a manipulação de classes (adicionar, remover, alternar, verificar) individualmente, sem afetar outras classes existentes no elemento, 
+    // sendo mais prática que o classNam
 
 //função em seta 
 const calcularDesconto = (preco, desconto) => preco * desconto / 100
-
 
 // function calcularDesconto(preco, desconto) {
 //     const valorEconomizado = preco * desconto / 100
@@ -25,14 +27,11 @@ const calcularDesconto = (preco, desconto) => preco * desconto / 100
 function mostrarResultados (){
     const preco = Number(document.getElementById('preco').value)
     const desconto = Number(document.getElementById('desconto').value)
-    const resultado = document.getElementById('resultado')
+    const resultado = (document.getElementById('resultado'))
     calcularDesconto(preco, desconto)
     resultado.textContent = calcularDesconto(preco,desconto)
     trocarCor()
-
 }
 
 document.getElementById('calcular')
         .addEventListener('click', mostrarResultados)
-
-
